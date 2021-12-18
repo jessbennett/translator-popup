da845400fb3727bf2e7f2aca39a8cc306e7244e4
Jess <jessbennett924@gmail.com>
2021-12-18 05:07:09 -0500

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+
+// import { Dialog } from '@material-ui/core/Dialog';
+
+// const AlertModal = (props) => (
+//   <Dialog aria-labelledby='alert-dialog-title'></Dialog>
+// );
+
+// export default AlertModal;
