0cdd46b17701c080d74c2a9ad450460681a12726
Jess <jessbennett924@gmail.com>
2021-12-18 05:40:30 -0500

    :added dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import './App.css';
 import Card from './components/Card';
-import { CardData } from './components/CardData';
+// import { CardData } from './components/CardData';
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Card />;
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,15 @@
-// import React from 'react';
+import React from 'react';
+import { UserCard } from 'react-ui-cards';
 
-// import { Dialog } from '@material-ui/core/Dialog';
+const Card = () => (
+  <UserCard
+    float
+    href='https://github.com/nukeop'
+    header='https://i.imgur.com/vRAtM3i.jpg'
+    avatar='https://i.imgur.com/XJxqvsU.jpg'
+    name='Frank Hepsfield'
+    positionName='Software Engineering Manager'
+  />
+);
 
-// const AlertModal = (props) => (
-//   <Dialog aria-labelledby='alert-dialog-title'></Dialog>
-// );
-
-// export default AlertModal;
+export default Card;
